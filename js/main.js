8332861d8416dfd28ba0c457f144b82a6a04f004
Dejan Mandić <32017278+scantarius@users.noreply.github.com>
2023-05-01 15:50:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".carousel");
+    var options = {};
+    var instances = M.Carousel.init(elems, options);
+});
